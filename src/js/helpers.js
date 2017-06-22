59bd10585955306cb0b2971aad460ec37ca25b83
Alvin Chan <aschan@gmail.com>
2017-06-21 18:27:24 -0700

    Add a `forEach()` helper

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,3 +1,9 @@
 /* eslint-disable no-unused-vars */
 const $ = (target, context) => (context || document).querySelector(target)
 const $$ = (target, context) => (context || document).querySelectorAll(target)
+
+const forEach = (array, callback, scope) => {
+  for (let i = 0, limit = array.length; i < limit; i++) {
+    callback.call(scope, array[i], i)
+  }
+}
